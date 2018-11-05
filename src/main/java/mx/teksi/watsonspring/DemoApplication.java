ed3f17a8b9565e92053731bcb0cce2ecbba13219
Jorge <jorge@teksi.mx>
2018-11-05 17:43:59 -0600

    commit inicial

---
+++ src/main/java/mx/teksi/watsonspring/DemoApplication.java
@@ -0,0 +1,37 @@
+package mx.teksi.watsonspring;
+
+import java.util.stream.Collectors;
+
+import com.ibm.watson.developer_cloud.discovery.v1.Discovery;
+import com.ibm.watson.developer_cloud.discovery.v1.model.QueryOptions;
+import com.ibm.watson.developer_cloud.discovery.v1.model.QueryResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+public class DemoApplication {
+	@Autowired
+	protected Discovery discovery;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value="/discover")
+	public String getMethodName(@RequestParam String query) {
+		QueryOptions options = new QueryOptions.Builder("system","news")
+		.naturalLanguageQuery(query)
+		.build();
+		QueryResponse queryResponse = discovery.query(options).execute();
+		String titles = queryResponse.getResults().stream()
+		.map(r ->(String)r.get("title"))
+		.collect(Collectors.joining("\n<p>"));
+		return titles;
+	}
+	
+}
